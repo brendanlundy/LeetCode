6314f04c925379b35e63d4955376a2d677672e8b
Brendan Lundy <brendan.lundy12@gmail.com>
2015-07-07 16:29:47 -0400

    search insert position

---
+++ algorithms/Problem035.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+public class Problem035 {
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[] { 5, 7, 8, 10 }, 1));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        int mid = -1;
+        while (left <= right) {
+            mid = left + ((right - left) / 2);
+            int num = nums[mid];
+            if (target > num) {
+                left = mid + 1;
+            } else if (target < num) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        if (target > nums[mid])
+            return mid + 1;
+        else
+            return mid;
+
+    }
+}
