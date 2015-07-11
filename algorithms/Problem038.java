1e97816241c535a2f674833b77d27f4ee18ba714
Brendan Lundy <brendan.lundy12@gmail.com>
2015-07-11 16:52:21 -0400

    count and say

--- algorithms/Problem038.java
+++ algorithms/Problem038.java
@@ -7,7 +7,29 @@ public class Problem038 {
     }
 
     public static String countAndSay(int n) {
-
-        return "11";
+        String answer = "1";
+        for (int i = 1; i < n; i++) {
+            int length = answer.length();
+            StringBuilder sb = new StringBuilder();
+            int j = 0;
+            int count = 0;
+            char currentChar = answer.charAt(0);
+            while (j < length) {
+                char nextChar = answer.charAt(j);
+                if (nextChar == currentChar) {
+                    count++;
+                } else {
+                    sb.append(Integer.toString(count));
+                    sb.append(currentChar);
+                    count = 1;
+                    currentChar = nextChar;
+                }
+                j++;
+            }
+            sb.append(Integer.toString(count));
+            sb.append(currentChar);
+            answer = sb.toString();
+        }
+        return answer;
     }
 }
